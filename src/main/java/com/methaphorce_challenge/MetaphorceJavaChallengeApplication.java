3c8206c2e3a76bd61ca3ff6d38c5f2a888340839
Panch094 <62124664+Panch094@users.noreply.github.com>
2022-07-30 15:29:29 -0500

    Upload the new files

---
+++ src/main/java/com/methaphorce_challenge/MetaphorceJavaChallengeApplication.java
@@ -0,0 +1,14 @@
+package com.methaphorce_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaphorceJavaChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetaphorceJavaChallengeApplication.class, args);
+		System.out.println("RUNNING PROJECT...");
+	}
+
+}
---
+++ src/test/java/com/methaphorce_challenge/MetaphorceJavaChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.methaphorce_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetaphorceJavaChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
